1a612d160d9e40f8f9173d8eec25414332699378
R0b0to <atonrhai@gmail.com>
2026-06-19 10:56:35 +0200

    FFmpeg backend

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:fvp/fvp.dart' as fvp;
 
 import 'theme.dart';
 import 'screens/dashboard/vault_dashboard.dart';
 
 void main() {
+  fvp.registerWith(options: {
+    'platforms': ['android'],
+  });
+
   runApp(const CryptBridgeApp());
 }
 

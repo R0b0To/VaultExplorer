28329234c0cac1a57151672d9aaf7dc8575d16db
R0b0to <atonrhai@gmail.com>
2026-09-22 23:23:55 +0200

    refactor: replace 'flutter/material.dart' imports with 'material_ui/material_ui.dart' across multiple test files

--- test/features/browser/viewer/video_scrub_fullscreen_layer_test.dart
+++ test/features/browser/viewer/video_scrub_fullscreen_layer_test.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'dart:typed_data';
 
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
+import 'package:material_ui/material_ui.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:vaultexplorer/features/browser/viewer/native_video_controller.dart';
 import 'package:vaultexplorer/features/browser/viewer/video_scrub_preview_controller.dart';
--- test/features/browser/widgets/fast_scrollbar_test.dart
+++ test/features/browser/widgets/fast_scrollbar_test.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:material_ui/material_ui.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:vaultexplorer/core/utils/raw_entry.dart';
 import 'package:vaultexplorer/features/browser/mixins/sort_mixin.dart';
